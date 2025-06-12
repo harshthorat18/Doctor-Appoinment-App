4173b8f4d5e277f4148988557dcbd3b5070b0553
harsh <harshthorat18@gmail.com>
2025-06-12 10:17:39 +0530

    Fix: Added missing cors import

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const moragan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cors = require('cors');
 
 //dotenv conig
 dotenv.config();
